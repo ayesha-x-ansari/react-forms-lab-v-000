fb988c5ee7ee1e9a71049316511aa4d1016c5444
ayesha ansari <ayesha.x.ansari@gmail.com>
2018-11-05 01:58:42 +0000

    automatically backed up by learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,23 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 class LoginForm extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      username: '',
-      password: '',
+      username: "",
+      password: "",
     };
   }
 
-
-  handleChange = event => {
+  handleInputChange = event => {
+    const { name, value } = event.target;
+    console.log(name);
     this.setState({
-      [event.target.name]: event.target.value,
+      [name]: value,
     });
-  }
+  };
 
-  handleSubmit = event => {
+  handleFormSubmit = event => {
     event.preventDefault();
     const { username, password } = this.state;
 
@@ -28,28 +30,43 @@ class LoginForm extends React.Component {
     this.props.onSubmit({ username, password });
   };
 
-
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <form onSubmit={this.handleFormSubmit}>
         <div>
           <label>
             Username
-            <input id="test-username" type="text" name="username" value={this.state.username} onChange={this.handleChange} />
+            <input
+              id="test-username"
+              type="text"
+              name="username"
+              value={this.state.username}
+              onChange={this.handleInputChange}
+            />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="test-password" type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+            <input
+              id="test-password"
+              type="password"
+              name="password"
+              value={this.state.password}
+              onChange={this.handleInputChange}
+            />
           </label>
         </div>
         <div>
-          <button type="submit" >Log in</button>
+          <button type="submit">Log in</button>
         </div>
       </form>
     );
   }
 }
+
+LoginForm.propTypes = {
+  onSubmit: PropTypes.func,
+};
 
 export default LoginForm;
